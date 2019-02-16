bd2b1e01bc576ccb3ff9ccd6b71be8673a205325
Evan Suther <esuther@ucsc.edu>
2019-02-16 15:58:27 -0800

    starting to do main cases

--- asg3-listmap-templates/code/main.cpp
+++ asg3-listmap-templates/code/main.cpp
@@ -6,8 +6,10 @@
 #include <cstdlib>
 #include <exception>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <unistd.h>
+#include <regex>
 
 using namespace std;
 
@@ -38,14 +40,52 @@ void scan_options (int argc, char** argv) {
 int main (int argc, char** argv) {
    sys_info::execname (argv[0]);
    scan_options (argc, argv);
-
+   regex comment_regex {R"(^\s*(#.*)?$)"};
+   regex key_value_regex {R"(^\s*(.*?)\s*=\s*(.*?)\s*$)"};
+   regex trimmed_regex {R"(^\s*([^=]+?)\s*$)"};
    str_str_map test;
+   DEBUGF('s', "debug test" << endl);
+   //will not enter loop if no files are given
    for (char** argp = &argv[optind]; argp != &argv[argc]; ++argp) {
-      str_str_pair pair (*argp, to_string<int> (argp - argv));
-      cout << "Before insert: " << pair << endl;
-      test.insert (pair);
+      //test.prompt(argp);
+      std::fstream fs;
+      string curr = *argp;
+      string line;
+      fs.open (curr, std::fstream::ios_base::in);
+      int counter = 1;
+      if(fs.is_open()){
+         while ( getline (fs, line) ){
+            //read each file line by line
+            if (fs.eof()) break;
+            cout << argp << ": "<< counter << ": " << line << endl;
+            ++counter;
+            smatch result;
+            //if found comment line
+            if (regex_search (line, result, comment_regex)) {
+               cout << "Comment or empty line." << endl;
+               DEBUGF('s', "Comment or empty line" << endl);
+               continue;
+            }
+            //if found '=' in line
+            if (regex_search (line, result, key_value_regex)) {
+               cout << "key  : \"" << result[1] << "\"" << endl;
+               cout << "value: \"" << result[2] << "\"" << endl;
+            }
+            //need to check if this is a key
+            else if (regex_search (line, result, trimmed_regex)) {
+               cout << "query: \"" << result[1] << "\"" << endl;
+            }
+         }
+      }else{// file did not print
+         //cerr << complain();
+      }
+      //str_str_pair pair (*argp, to_string<int> (argp - argv));
+      //cout << "Before insert: " << pair << endl;
+      //test.insert (pair);
+      fs.close();
    }
 
+   //must have things in listmap first to enter???????????????????????
    for (str_str_map::iterator itor = test.begin();
         itor != test.end(); ++itor) {
       cout << "During iteration: " << *itor << endl;
